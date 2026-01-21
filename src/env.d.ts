a7a4eef2467b6b01b10637d8157ce8438044c2d3
DL21hex <diego.llinas@gmail.com>
2026-01-20 20:50:15 -0500

    feat: add GitHub Actions workflow for frontend deployment and configure environment bindings

---
+++ src/env.d.ts
@@ -0,0 +1,3 @@
+interface Env {
+    NX32_CORE: Fetcher;
+}
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -1,34 +1,41 @@
 import { Component, createEffect } from "solid-js";
 import { action, useSubmission} from "@solidjs/router";
 import { getRequestEvent } from "solid-js/web";
-import { getApiBaseUrl } from "~/constants/api";
 
 const loginAction = action(async (formData: FormData) => {
 	"use server";
 	const event = getRequestEvent();
-	const apiBaseUrl = getApiBaseUrl(event);
 
-	try {
-		//console.log('Feching login API:', `${apiBaseUrl}/system/users/authenticate_public`);
-		const response = await fetch(`${apiBaseUrl}/system/users/authenticate_public`, {
+	const env = event?.nativeEvent.context.cloudflare.env as Env;
+
+	if (!env || !env.NX32_CORE)
+	{
+		throw new Error("El servicio Core no está conectado (Binding missing).");
+	}
+
+	try
+	{
+		const payload = Object.fromEntries(formData);
+		const request = new Request("https://dummy.nx32.app", {
 			method: "POST",
 			headers: {
-    			"Content-Type": "application/json"
+				"Content-Type": "application/json"
 			},
-			body: JSON.stringify(Object.fromEntries(formData))
+			body: formData,
 		});
+
+		const response = await env.NX32_CORE.fetch(request);
 
 		if (!response.ok)
 		{
 			const errorText = await response.text();
+			console.error('Login failed:', response.status, errorText);
 			throw new Error(`Credenciales inválidas (${response.status}): ${errorText}`);
 		}
 
 		const data = await response.json();
-
 		const setCookie = response.headers.get("Set-Cookie");
-		console.log(setCookie);
-		const headers = new Headers();
+
 		if (setCookie && event)
 		{
 			event.response.headers.append("Set-Cookie", setCookie);
@@ -38,8 +45,8 @@ const loginAction = action(async (formData: FormData) => {
 	}
 	catch (error)
 	{
-		console.error("Network or server error during login:", error);
-		throw error;
+		console.error("Error conectando con el Core:", error);
+        throw error;
 	}
 });
 
